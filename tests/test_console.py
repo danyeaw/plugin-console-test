2e5c8f1bd35577f244a2c1f807da15daffd350cd
Dan Yeaw <dan@yeaw.me>
2019-12-15 15:23:13 -0500

    Revert "Remove gi import"
    This reverts commit 83a6f55a
    
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,3 +1,9 @@
+import gi
+
+gi.require_version("Gdk", "3.0")
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gdk, Gtk
+
 from plugin.console import GTKInterpreterConsole, Help, main
 
 
