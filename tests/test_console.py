967a71ec8aa129d01a1c42759ea5142458acced3
Dan Yeaw <dan@yeaw.me>
2019-12-15 15:02:47 -0500

    Add back in KeyEvent class
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,10 +1,10 @@
 from plugin.console import Help
 
 
-# class KeyEvent:
-#    def __init__(self, keyval, state=0):
-#        self.keyval = keyval
-#        self.state = state
+class KeyEvent:
+    def __init__(self, keyval, state=0):
+        self.keyval = keyval
+        self.state = state
 
 
 def test_help():
