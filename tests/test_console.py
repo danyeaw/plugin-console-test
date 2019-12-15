e585b12f073e3b22f416eae31250d02dbb7ce0ef
Dan Yeaw <dan@yeaw.me>
2019-12-15 15:17:40 -0500

    Remove KeyEvent class
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- tests/test_console.py
+++ tests/test_console.py
@@ -7,12 +7,6 @@ from gi.repository import Gdk, Gtk
 from plugin.console import GTKInterpreterConsole, Help, main
 
 
-class KeyEvent:
-    def __init__(self, keyval, state=0):
-        self.keyval = keyval
-        self.state = state
-
-
 def test_help():
      help = Help()
 
