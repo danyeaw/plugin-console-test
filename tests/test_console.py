41fae3385dd68bd3bd92b1c018ef665e15e96579
Dan Yeaw <dan@yeaw.me>
2019-12-15 17:00:32 -0500

    Remove GTKInterpretiveConsole import
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- tests/test_console.py
+++ tests/test_console.py
@@ -4,12 +4,10 @@ gi.require_version("Gdk", "3.0")
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gdk, Gtk
 
-from plugin.console import GTKInterpreterConsole, Help, main
+from plugin.console import Help, main
 
 
 def test_help():
      help = Help()
 
      assert help() == "Usage: help(object)"
-
-
