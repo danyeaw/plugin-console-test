4f85101ca1ddf75954256a4c1c9c2cf76d551b70
Dan Yeaw <dan@yeaw.me>
2021-11-25 12:14:17 -0500

    Remove Gtk from console module

--- plugin/console.py
+++ plugin/console.py
@@ -1,8 +1,4 @@
-import gi
 from rlcompleter import Completer
-
-gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
 
 
 class Help:
@@ -11,14 +7,3 @@ class Help:
 
     def __str__(self):
         return "Usage: help(object)"
-
-
-def main():
-    w = Gtk.Window()
-    w.show()
-    w.connect("destroy", Gtk.main_quit)
-    Gtk.main()
-
-
-if __name__ == "__main__":
-    main()
