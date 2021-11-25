570cd2162f44d82a7ff5625ee095cbeb6fbb775a
Dan Yeaw <dan@yeaw.me>
2021-11-25 18:27:06 -0500

    Enable faulthandler

--- tests/test_console.py
+++ tests/test_console.py
@@ -4,7 +4,10 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
 from plugin.console import get_four
+import faulthandler
 
+
+faulthandler.enable()
 
 def test_equal():
     assert get_four() == 4
