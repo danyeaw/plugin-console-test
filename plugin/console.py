760c397b5f3f72b0ed205365dd3ad0c5d25be874
Dan Yeaw <dan@yeaw.me>
2021-11-25 12:20:53 -0500

    Remove Help class

--- plugin/console.py
+++
@@ -1,9 +0,0 @@
-from rlcompleter import Completer
-
-
-class Help:
-    def __call__(self):
-        return str(self)
-
-    def __str__(self):
-        return "Usage: help(object)"
--- tests/test_console.py
+++ tests/test_console.py
@@ -1,12 +1,10 @@
+from rlcompleter import Completer
+
 import gi
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
-from plugin.console import Help
 
-
-def test_help():
-     help = Help()
-
-     assert help() == "Usage: help(object)"
+def test_answer():
+    assert 5 == 5
